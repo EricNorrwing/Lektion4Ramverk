6d516ce24c6debe8f8c39bef533b02d1a5c395f7
EricNorrwing <Eric.Norrwing@gmail.com>
2024-02-06 09:03:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const addera = (a, b) => {
+    return a + b;
+}
+
+console.log(addera(5 + 7));
+
+const isAccepted = !false;
+
+if (isAccepted) {
+    console.log("Accepterad")
+} else {
+    console.log("Inte accepterad")
+}
+
+let grade = 80;
+
+if (grade > 50 && grade < 75) {
+    console.log("Betyg: E");
+} else if (grade >= 75 && grade < 90) {
+    console.log("Betyg: C");
+} else if (grade > 90) {
+    console.log("Betyg: A");
+} else {
+    console.log("Betyg: F");
+}
+
+const day = "Friday";
+
+switch (day) {
+    case "Friday":
+        console.log("FREEDAG");
+        break;
+    case "Saturday":
+        console.log("VAFAN DET E HELG");
+        break;
+    case "Söndag":
+        console.log("Snooze ZZzzZZZzZZ");
+        break;
+    default:
+        console.log("VA E DETTA JOBBE ELLER");
+}
+
+
+/* const myFunctions = {
+    firstFunction: (name) => {
+        console.log(name)
+    },
+    secondFunction: (name) => console.log("name"),
+} THIS FUNCTION DOESNT HAVE OUTPUT, ITS SIMPLY A DISPLAY OF DIFFERENCES OF SYNTAX*/
+
+/* for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+{
+let i = 1
+while (i <= 5){
+    console.log(i);
+    i++;
+}
+let fruits = ["apple", "banana", "orange", "strawberry"];
+
+for(let fruit of fruits) {
+    console.log(fruit)
+}
+}
+
+let numbers = [5, 12, 18, 22, 3];
+
+numbers = numbers.filter((number) => number > 10);
+console.log(numbers)
+
+let people =[
+    {name: "John", age: 30},
+    {name: "Sara", age: 27},
+    {name: "Daniel", age: 19}
+]
+
+const sortedPeople = people.sort((a,b) => a.age + b.age)
+for (let i of sortedPeople){
+    console.log(i)
+} */
